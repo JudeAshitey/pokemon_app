860608344ae4a64ceb0ef2dfa4ba1bf9d6e6305a
JudeAshitey <judeashitey4@gmail.com>
2022-10-11 22:21:55 -0400

    Mod2 Homework

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const pokemon = require("./models/pokemon")
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended:false}))
+app.set("view engine","jsx")
+app.engine("jsx",require("express-react-views").createEngine())
+
+app.get('/', (req,res) =>{
+    res.send('Welcome to the Pokemon App')
+})
+
+app.get("/pokemon", (req,res) =>{
+    res.render("Index", {pokemon})
+})
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running ${PORT}`);
+})
